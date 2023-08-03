7bda806f96492b13410e70e1d6b2867ea7d86033
unknown <raihan2-byte>
2023-08-03 20:54:20 +0700

    bug

--- handler/artikel.go
+++ handler/artikel.go
@@ -3,10 +3,10 @@ package handler
 import (
 	"blog/artikel"
 	"blog/helper"
-	"blog/user"
 	"bytes"
 	"context"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -99,10 +99,18 @@ func (h *artikelHandler) CreateArtikel (c *gin.Context){
 
 	fmt.Println("image base 64 format : %v",img)
 
-	x(context.Background(),img)
+	imageKitURL, err := x(context.Background(), img)
+	if err != nil {
+		// Tangani jika terjadi kesalahan saat upload gambar
+		// Misalnya, Anda dapat mengembalikan respon error ke klien jika diperlukan
+		response := helper.APIresponse(http.StatusInternalServerError, "Failed to upload image")
+		c.JSON(http.StatusInternalServerError, response)
+		return
+	}
 	
 
 	var input artikel.CreateArtikel
+	
 
 	err = c.ShouldBind(&input)
 
@@ -122,22 +130,32 @@ func (h *artikelHandler) CreateArtikel (c *gin.Context){
 		return
 	}
 
-	currentUser := c.MustGet("currentUser").(user.User)
-	//ini inisiasi userID yang mana ingin mendapatkan id si user
-	// input.User = currentUser
-	userID := currentUser.ID
+	// currentUser := c.MustGet("currentUser").(user.User)
+	// //ini inisiasi userID yang mana ingin mendapatkan id si user
+	// // input.User = currentUser
+	// userID := currentUser.ID
 
-	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
+	// path := fmt.Sprintf(imageKitURL)
 
-	err = c.SaveUploadedFile(file, path)
-	if err != nil {
-		data := gin.H{"is_uploaded": false}
-		response := helper.APIresponse(http.StatusUnprocessableEntity, data)
-		c.JSON(http.StatusUnprocessableEntity, response)
-		return
-	}
+	// input.FileName = imageKitURL
 
-	_, err = h.artikelService.CreateArtikel(input, path)
+	// artikel := Artikel{
+	// 	Judul:          input.Judul,
+	// 	ArtikelMessage: input.ArtikelMessage,
+	// 	FileName:       file.Filename, // Set nilai FileName dengan nama file asli yang diunggah
+	// 	CreatedAt:      time.Now(),
+	// 	UpdatedAt:      time.Now(),
+	// }
+
+	// err = c.SaveUploadedFile(file, imageKitURL)
+	// if err != nil {
+	// 	// data := gin.H{"is_uploaded": false}
+	// 	response := helper.APIresponse(http.StatusUnprocessableEntity, err)
+	// 	c.JSON(http.StatusUnprocessableEntity, response)
+	// 	return
+	// }
+
+	_, err = h.artikelService.CreateArtikel(input, imageKitURL)
 	if err != nil {
 		// data := gin.H{"is_uploaded": false}
 		response := helper.APIresponse(http.StatusUnprocessableEntity, err)
@@ -176,31 +194,36 @@ func toBase64(b []byte) string {
 	return base64.StdEncoding.EncodeToString(b)
 }
 
-func x(ctx context.Context, base64Image string){
+func x(ctx context.Context, base64Image string) (string, error) {
 	fmt.Println("start uploading image ...")
 	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
-    defer cancel()
-	ik:=imagekit.NewFromParams(imagekit.NewParams{
-		PrivateKey: "private_iitVYNY2fbOQSJgHtSccK9agJz0=",
-		PublicKey: "public_OtKeno1x/kY3zk5m7I9eNwnAtrY=",
+	defer cancel()
+	ik := imagekit.NewFromParams(imagekit.NewParams{
+		PrivateKey:  "private_iitVYNY2fbOQSJgHtSccK9agJz0=",
+		PublicKey:   "public_OtKeno1x/kY3zk5m7I9eNwnAtrY=",
 		UrlEndpoint: "https://ik.imagekit.io/raihan2",
 	})
-	
-	resp,err:=ik.Uploader.Upload(ctx,base64Image,uploader.UploadParam{
+
+	resp, err := ik.Uploader.Upload(ctx, base64Image, uploader.UploadParam{
 		FileName: "test_image.jpg",
-		Tags: "barang",
-		Folder: "unj",
+		Tags:     "barang",
+		Folder:   "unj",
 	})
 
-	if err!=nil{
-		fmt.Printf("an error occurred when uploading image %v",err)
+	if err != nil {
+		fmt.Printf("an error occurred when uploading image %v", err)
+		return "", err
 	}
 
-	if resp.StatusCode!=200{
-		fmt.Printf("an error occurred when uploading image %v",resp)
+	if resp.StatusCode != 200 {
+		fmt.Printf("an error occurred when uploading image %v", resp)
+		return "", errors.New("failed to upload image")
 	}
 
+	// Return the ImageKit URL
+	return resp.Data.Url, nil
 }
+
 
 func (h *artikelHandler) GetAllArtikel(c *gin.Context){
 	input, _ := strconv.Atoi(c.Query("id"))
